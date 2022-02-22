b1600cc6e330547cbac0cc7f18b6a6738d87a206
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2022-02-22 16:49:49 +0700

    update

--- lavadurian/settings.py
+++ lavadurian/settings.py
@@ -115,11 +115,14 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ssk_durain',
+        'USER': 'phisan',
+        'PASSWORD': 'numvarnSc282456',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
