c87bb9b07c063bbedb01ea667e2a57acfd6fd701
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2022-02-24 04:52:49 +0700

    remove csrf

--- lavadurian/settings.py
+++ lavadurian/settings.py
@@ -70,13 +70,15 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'lavadurian.urls'
+
+# CSRF_COOKIE_SECURE = True
 
 TEMPLATES = [
     {
