d9663887151a28dc1735255443e8b02764c8953c
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2023-06-01 09:10:32 +0000

    update manage.py

--- manage.py
+++ manage.py
@@ -6,10 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lavadurian.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lavadurian.settings')
 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'lavadurian.settings_local')
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+    #                      'lavadurian.settings_local')
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
