7974fab6f97a6da4b8756155ab40fabf8353ca9d
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2023-06-01 15:03:20 +0700

    update gitignore

--- manage.py
+++ manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lavadurian.settings')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lavadurian.settings')
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'lavadurian.settings_local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
