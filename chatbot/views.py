5310df9d474ca13ef6b1be6b0ee8430369c9bd81
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2023-06-07 21:08:32 +0700

    update

--- chatbot/views.py
+++ chatbot/views.py
@@ -82,7 +82,7 @@ def replyProfile(reply_token, disname, text):
     text_lt = text.split(":")
     store_id = text_lt[1].strip()
 
-    store = Store.objects.filter(id=int(store_id))
+    # store = Store.objects.filter(id=int(store_id))
 
     flex_str = """
 {
@@ -100,7 +100,7 @@ def replyProfile(reply_token, disname, text):
       },
       {
         "type": "text",
-        "text": "%s",
+        "text": "กุญชรเพชรทุเรียนภูเขาไฟ %s",
         "weight": "bold",
         "size": "xxl",
         "margin": "md",
@@ -273,7 +273,7 @@ def replyProfile(reply_token, disname, text):
     }
   }
 }
-    """ % (store.name)
+    """ % (store_id)
 
     flex = json.loads(flex_str)
     replyObj = FlexSendMessage(alt_text='Flex Message alt text', contents=flex)
