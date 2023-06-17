a181cc95f6ee65031ae2349992d99e5698c51cab
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2023-06-17 12:39:08 +0700

    update

--- chatbot/views.py
+++ chatbot/views.py
@@ -989,7 +989,7 @@ def replyNews(reply_token, disname):
                 "flex": 0
             }
         }
-        ''' % (news.image.url, news.id, news.title)
+        ''' % (news.image.url, news.id, news.title, )
 
         flex_lt.append(flex_str)
 
